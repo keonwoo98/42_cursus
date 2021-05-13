3eae5ffdde2061380af1511766be896723d3389f
Keonwoo Kim <keokim@c1r4s7.42seoul.kr>
2021-05-13 19:31:58 +0900

    Libft

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,29 +6,26 @@
 /*   By: keokim <keokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 10:53:02 by keokim            #+#    #+#             */
-/*   Updated: 2021/05/06 12:56:57 by keokim           ###   ########.fr       */
+/*   Updated: 2021/05/13 18:33:17 by keokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char		*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	len_s;
-	char	*sub_s;
+	char	*substr;
+	size_t	new_len;
 
 	if (s == NULL)
 		return (NULL);
-	if (!(sub_s = (char *)malloc(sizeof(char) * (len + 1))))
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	new_len = ft_strlen(s + start);
+	if (new_len < len)
+		len = new_len;
+	if (!(substr = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	len_s = ft_strlen(s);
-	i = 0;
-	while (i < len && i + start < len_s)
-	{
-		sub_s[i] = s[start + i];
-		i++;
-	}
-	sub_s[i] = '\0';
-	return (sub_s);
+	ft_strlcpy(substr, s + start, len + 1);
+	return (substr);
 }
