2ac903f7f0f8e185a5800b28d8d4695e78216c98
Keonwoo Kim <keokim@c1r4s7.42seoul.kr>
2021-05-20 18:53:14 +0900

    Get_Next_Line

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,10 +6,20 @@
 /*   By: keokim <keokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 12:33:29 by keokim            #+#    #+#             */
-/*   Updated: 2021/05/18 12:34:38 by keokim           ###   ########.fr       */
+/*   Updated: 2021/05/20 13:42:27 by keokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line)
+{
+	int			read_size;
+	char		buf[BUFFER_SIZE + 1];
+	static		*backup[OPEN_MAX];
+
+	if ((fd < 0) || !line || (BUFFER_SIZE <= 0))
+		return (-1);
+	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[read_size] = '\0';
--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -6,8 +6,17 @@
 /*   By: keokim <keokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 19:54:48 by keokim            #+#    #+#             */
-/*   Updated: 2021/05/18 19:54:51 by keokim           ###   ########.fr       */
+/*   Updated: 2021/05/20 14:51:31 by keokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
+
+int			get_next_line(int fd, char **line);
+
+#endif
