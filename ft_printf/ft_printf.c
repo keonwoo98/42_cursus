5914c7be347037d94799c49828d5a38c89f170d9
keonwoo98 <keonwoo98@naver.com>
2021-06-14 11:39:03 +0900

    ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -44,44 +44,44 @@ int			    ft_printf(const char *str, ...)
     return (ret);
 }
 
-int main()
-{
-    unsigned int u = 4294967161;
+// int main()
+// {
+//     unsigned int u = 4294967161;
 
-    ft_printf("%3.7i\n", 3267);
-    ft_printf("%3.7d\n", 3267);
-    ft_printf("%3.7d\n", 3267);
-    ft_printf("-->|%-4.*u|<--\n", -4, u);
-    ft_printf("-->|%#-4.3x|<--\n", 1);
-    ft_printf("%.3d\n", 10);
-    ft_printf("%.3d\n", 10000);
-    ft_printf("%.0d\n", 0);
-    ft_printf("%.5s\n", "Hello 42!!");
-    ft_printf("%.5s\n", "abc");
+//     ft_printf("%3.7i\n", 3267);
+//     ft_printf("%3.7d\n", 3267);
+//     ft_printf("%3.7d\n", 3267);
+//     ft_printf("-->|%-4.*u|<--\n", -4, u);
+//     ft_printf("-->|%#-4.3x|<--\n", 1);
+//     ft_printf("%.3d\n", 10);
+//     ft_printf("%.3d\n", 10000);
+//     ft_printf("%.0d\n", 0);
+//     ft_printf("%.5s\n", "Hello 42!!");
+//     ft_printf("%.5s\n", "abc");
 
-    ft_printf("%1s\n", "abc");
+//     ft_printf("%1s\n", "abc");
 
-    ft_printf("%#x\n", 10);
-    ft_printf("%#X\n", 16);
-    ft_printf("%#o\n", 8);
+//     ft_printf("%#x\n", 10);
+//     ft_printf("%#X\n", 16);
+//     ft_printf("%#o\n", 8);
     
-    ft_printf("%+d\n", 10);
-    ft_printf( "%-d\n", 15);
+//     ft_printf("%+d\n", 10);
+//     ft_printf( "%-d\n", 15);
 
-    ft_printf("%%\n");
-    ft_printf("%#+09.5lf\n", 1.7E+3);
-    ft_printf("%-10.10s\n","truncateiftoolong");
-    ft_printf("%8.5f\n", 1.234 );
+//     ft_printf("%%\n");
+//     ft_printf("%#+09.5lf\n", 1.7E+3);
+//     ft_printf("%-10.10s\n","truncateiftoolong");
+//     ft_printf("%8.5f\n", 1.234 );
 
-    ft_printf("%8.5d\n", 1234 );
-    ft_printf("%5s\n", "abc");
-    ft_printf("%05d\n", 10);
-    ft_printf("%#012x\n", 0xff);
+//     ft_printf("%8.5d\n", 1234 );
+//     ft_printf("%5s\n", "abc");
+//     ft_printf("%05d\n", 10);
+//     ft_printf("%#012x\n", 0xff);
 
-    ft_printf("ft_printf : %*p%-135.109d\n" ,-15,(void*)15603137398285451084lu,1482420587);
-    ft_printf("ft_printf : %*p\n" ,-15,(void*)15603137398285451);
-    // ft_printf("%21747483648.2147483648d", 125);
-    ft_printf("%120p\n" ,(void*)9377650025287837451lu);
+//     ft_printf("ft_printf : %*p%-135.109d\n" ,-15,(void*)15603137398285451084lu,1482420587);
+//     ft_printf("ft_printf : %*p\n" ,-15,(void*)15603137398285451);
+//     // ft_printf("%21747483648.2147483648d", 125);
+//     ft_printf("%120p\n" ,(void*)9377650025287837451lu);
 
-    while(1);
-}
+//     while(1);
+// }
