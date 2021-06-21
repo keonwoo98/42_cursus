fc186fa5a1e38ae29908850e9e5dcc93f93aca44
keonwoo98 <keonwoo98@naver.com>
2021-06-22 00:31:19 +0900

    Exam_02

--- Exam_02/GNL/get_next_line.c
+++ Exam_02/GNL/get_next_line.c
@@ -1,6 +1,25 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+char			*ft_strdup(const char *s)
+{
+	int			i;
+	int			len;
+	char		*str;
+
+	len = ft_strlen(s);
+	if (!(str = (char *)malloc(len + 1)))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
 int				ft_strlen(char *s)
 {
 	int			i = 0;
