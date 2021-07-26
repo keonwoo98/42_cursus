fe027083e2a9e4c26b92f0bf0bad712b4ef194ac
keonwoo98 <keonwoo98@naver.com>
2021-07-26 19:38:06 +0900

    push_swap

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,13 +1,19 @@
 #include "push_swap.h"
 
-int		main(int argc, char **argv)
+void
+	print_error(void)
+{
+	ft_putendl_fd("Error", 2);
+	exit(1);
+}
+
+int
+	main(int argc, char **argv)
 {
 		if (argc >= 2)
 		{
 
 		}
 		else
-		{
-
-		}
+			print_error();
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -3,4 +3,18 @@
 
 # include "./Libft/libft.h"
 
+typedef struct s_list
+{
+	int			value;
+	t_list		*prev;
+	t_list		*next;
+}t_list;
+
+typedef struct s_stack
+{
+	int			size;
+	t_stack		*top;
+	t_stack		*bottom;
+}t_stack;
+
 #endif
