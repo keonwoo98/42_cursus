d292821164569b8c089f3ee3bd34a90cfe972d38
keonwoo98 <keonwoo98@naver.com>
2021-10-25 18:14:06 +0900

    Philosophers

--- Philosophers/includes/philo.h
+++ Philosophers/includes/philo.h
@@ -10,12 +10,27 @@
 
 typedef struct s_philo
 {
-	int			num_of_philo;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			num_of_must_eat;
+	pthread_t			thread_id;
+	pthread_t			left_fork;
+	pthread_t			right_fork;
+	pthread_mutex_t		forks;
 }t_philo;
 
+typedef struct s_arg
+{
+	int					num_of_philo;		// 철학자의 수, 즉 포크의 갯수
+	int					time_to_die;		// 밀리세컨 단위, 철학자가 식사하지 않은지 time_to_die가 지나면 철학자는 죽는다
+	int					time_to_eat;		// 밀리세컨 단위, 철학자가 식사하는 시간, 포크 두 개 소유
+	int					time_to_sleep;		// 밀리세컨 단위, 철학자가 자는 시간
+	int					num_of_must_eat;	// 모든 철학자들이 이 횟수만큼 식사를 하게되면 프로그램 종료, 인자가 안들어오면 누군가 죽게되면 종료
+
+	long long			timestamp;
+
+	pthread_mutex_t		write_mutex;
+	pthread_mutex_t		dead;
+	pthread_mutex_t		time_eat;
+	pthread_mutex_t		finish;
+	t_philo				*philo;
+}t_arg;
 
 #endif
--- Philosophers/sources/main.c
+++ Philosophers/sources/main.c
@@ -1,18 +1,26 @@
 #include "philo.h"
 
 int
+	ft_strlen(char *str)
+{
+	int			i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int
 	error_msg(char *msg)
 {
-	int			len;
-
-	len = 0;
-	while (msg[len])
-		len++;
 	write(2, "Error: ", 7);
-	write(2, msg, len);
+	write(2, msg, ft_strlen(msg));
 	write(2, "\n", 1);
 	return (1);
 }
+
+
 
 int
 	ft_atoi(const char *str)
@@ -44,22 +52,103 @@ int
 }
 
 int
-	init_philo(t_philo *philo, char **argv)
+	is_num(char **argv)
 {
-	philo->num_of_philo = ft_atoi(argv[1]);
-	philo->time_to_die = ft_atoi(argv[2]);
-	philo->time_to_eat = ft_atoi(argv[3]);
-	philo->time_to_sleep = ft_atoi(argv[4]);
+	int			i;
+	int			j;
+
+	i = 1;
+	j = 0;
+	while (argv[i])
+	{
+		while (argv[i][j])
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				return (0);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (1);
+}
+
+int
+	init_arg(t_arg *arg, int argc, char **argv)
+{
+	if (!is_num(argv))
+		return (1);
+	arg->num_of_philo = ft_atoi(argv[1]);
+	arg->time_to_die = ft_atoi(argv[2]);
+	arg->time_to_eat = ft_atoi(argv[3]);
+	arg->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+	{
+		arg->num_of_must_eat = ft_atoi(argv[5]);
+		if (arg->num_of_must_eat <= 0)
+			return (1);
+	}
+	else
+		arg->num_of_must_eat = -1;
+	if (arg->num_of_philo <= 0 || arg->time_to_die <= 0 ||\
+		arg->time_to_eat <= 0 || arg->time_to_sleep <= 0)
+		return (1);
+	return (0);
+}
+
+long long
+	get_time(void)
+{
+	long long		ret;
+	struct timeval	time;
+
+	ret = 0;
+	if (gettimeofday(&time, NULL) == -1)
+		error_msg("gettimeofday fail");
+	ret = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+	return (ret);
+}
+
+int
+	init_mutex(t_philo *philo)
+{
+	if (pthread_mutex_init(&philo->forks, NULL))
+		return (1);
+}
+
+int
+	init_philo(t_arg *arg)
+{
+	int			i;
+
+	i = 0;
+	arg->timestamp = get_time();
+	pthread_mutex_init(&arg->write_mutex, NULL);
+	pthread_mutex_init(&arg->dead, NULL);
+	pthread_mutex_init(&arg->time_eat, NULL);
+	pthread_mutex_init(&arg->finish, NULL);
+	while (i < arg->num_of_philo)
+	{
+		if (init_mutex(&arg->philo[i]))
+			return (1);
+		i++;
+	}
 	return (0);
 }
 
 int
 	main(int argc, char **argv)
 {
-	t_philo		philo;
+	t_arg		arg;
 
 	if (argc != 5 && argc != 6)
 		return (error_msg("Wrong amount of arguments"));
-	if (init_philo(&philo, argv))
-		return (error_msg("Initializing"));
+	if (init_arg(&arg, argc, argv))
+		return (error_msg("Invalid arguments"));
+	arg.philo = (t_philo *)malloc(sizeof(t_philo) * arg.num_of_philo);
+	if (!arg.philo)
+		return (error_msg("Malloc fail"));
+	if (init_philo(&arg))
+		return (error_msg("Initialize fail"));
+	return (0);
 }
