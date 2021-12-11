8b29c3e10d7b22aff9e5c695c9c30e57f92f28a5
keonwoo98 <keonwoo98@naver.com>
2021-12-11 17:00:49 +0900

    Exam_03

--- Exam_03/micro_paint/micro_paint.c
+++ Exam_03/micro_paint/micro_paint.c
@@ -1,15 +1,15 @@
 #include "micro_paint.h"
 
 int
-	canva_dim_check(FILE *file, t_canva *canva_dim)
+	canva_check(FILE *file, t_canva *canva)
 {
 	int		ret;
 
-	ret = fscanf(file, "%d %d %c\n", &canva_dim->width, &canva_dim->height, &canva_dim->background);
+	ret = fscanf(file, "%d %d %c\n", &canva->width, &canva->height, &canva->background);
 	if (ret != 3)
 		return (0);
-	if (canva_dim->width <= 0 || canva_dim->width > 300 \
-	|| canva_dim->height <= 0 || canva_dim->height > 300)
+	if (canva->width <= 0 || canva->width > 300 \
+	|| canva->height <= 0 || canva->height > 300)
 		return (0);
 	if (ret == -1)
 		return (0);
@@ -17,57 +17,57 @@ int
 }
 
 char
-	**background_draw(t_canva canva_dim)
+	**background_draw(t_canva canva)
 {
 	int		i;
 	int		j;
 	int		k;
 	char	**buffer;
 
-	buffer = malloc(sizeof(*buffer) * canva_dim.height + 1);
-	for (k = 0; k < canva_dim.height; k++)
-		buffer[k] = malloc(sizeof(**buffer) * canva_dim.width);
+	buffer = malloc(sizeof(*buffer) * canva.height + 1);
+	for (k = 0; k < canva.height; k++)
+		buffer[k] = malloc(sizeof(**buffer) * canva.width);
 	buffer[k] = 0;
-	for (i = 0; i < canva_dim.height; i++)
+	for (i = 0; i < canva.height; i++)
 	{
-		for (j = 0; j < canva_dim.width; j++)
-			buffer[i][j] = canva_dim.background;
+		for (j = 0; j < canva.width; j++)
+			buffer[i][j] = canva.background;
 	}
 	return (buffer);
 }
 
 void
-	shape_draw(char **buffer, t_shape shape_dim, t_canva canva)
+	shape_draw(char **buffer, t_shape shape, t_canva canva)
 {
 	int		x;
 	int		y = 0;
 
-	if (shape_dim.type == 'R')
+	if (shape.type == 'R')
 	{
 		while (y < canva.height)
 		{
 			x = 0;
 			while (x < canva.width)
 			{
-				if (x >= shape_dim.x && x <= shape_dim.x + shape_dim.width && y >= shape_dim.y && y <= shape_dim.y + shape_dim.height)
-					buffer[y][x] = shape_dim.background;
+				if (x >= shape.x && x <= shape.x + shape.width && y >= shape.y && y <= shape.y + shape.height)
+					buffer[y][x] = shape.background;
 				x++;
 			}
 			y++;
 		}
 	}
-	else if (shape_dim.type == 'r')
+	else if (shape.type == 'r')
 	{
 		while (y < canva.height)
 		{
 			x = 0;
 			while (x < canva.width)
 			{
-				if (x >= shape_dim.x && x <= shape_dim.x + shape_dim.width && y >= shape_dim.y && y <= shape_dim.y + shape_dim.height)
+				if (x >= shape.x && x <= shape.x + shape.width && y >= shape.y && y <= shape.y + shape.height)
 				{
-					if (sqrtf(powf(x - shape_dim.x, 2.0)) < 1 || sqrtf(powf(shape_dim.x + shape_dim.width - x, 2.0)) < 1 \
-					|| sqrtf(powf(y - shape_dim.y, 2.0)) < 1 || sqrtf(powf(shape_dim.y + shape_dim.height - y, 2.0)) < 1)
-						buffer[y][x] = shape_dim.background;
+					if (sqrtf(powf(x - shape.x, 2.0)) < 1 || sqrtf(powf(shape.x + shape.width - x, 2.0)) < 1 \
+					|| sqrtf(powf(y - shape.y, 2.0)) < 1 || sqrtf(powf(shape.y + shape.height - y, 2.0)) < 1)
+						buffer[y][x] = shape.background;
 				}
 				x++;
 			}
@@ -77,18 +77,18 @@ void
 }
 
 int
-	final_draw_check(t_shape *shape_dim, FILE *file, char **buffer, t_canva canva_dim)
+	draw_check(t_shape *shape, FILE *file, char **buffer, t_canva canva)
 {
 	int		ret;
 
 	while ((ret = fscanf(file, "%c %f %f %f %f %c\n", \
-	&shape_dim->type, &shape_dim->x, &shape_dim->y, &shape_dim->width, &shape_dim->height, &shape_dim->background)) == 6)
+	&shape->type, &shape->x, &shape->y, &shape->width, &shape->height, &shape->background)) == 6)
 	{
-		if (shape_dim->width <= 0.0 || shape_dim->height <= 0.0)
+		if (shape->width <= 0.0 || shape->height <= 0.0)
 			return (0);
-		if (shape_dim->type != 'R' && shape_dim->type != 'r')
+		if (shape->type != 'R' && shape->type != 'r')
 			return (0);
-		shape_draw(buffer, *shape_dim, canva_dim);
+		shape_draw(buffer, *shape, canva);
 	}
 	if (ret != -1)
 		return (0);
@@ -96,9 +96,9 @@ int
 }
 
 void
-	free_buffer(t_canva canva_dim, char **buffer)
+	free_buffer(t_canva canva, char **buffer)
 {
-	for (int i = 0; i < canva_dim.height; i++)
+	for (int i = 0; i < canva.height; i++)
 		free(buffer[i]);
 	free(buffer);
 }
@@ -107,8 +107,8 @@ int
 	main(int argc, char **argv)
 {
 	FILE		*file;
-	t_canva		canva_dim;
-	t_shape		shape_dim;
+	t_canva		canva;
+	t_shape		shape;
 	char		**buffer;
 
 	if (argc != 2)
@@ -121,27 +121,27 @@ int
 		write(1, "Error: Operation file corrupted\n", 32);
 		return (1);
 	}
-	if (!canva_dim_check(file, &canva_dim))
+	if (!canva_check(file, &canva))
 	{
 		write(1, "Error: Operation file corrupted\n", 32);
 		fclose(file);
 		return (1);
 	}
-	buffer = background_draw(canva_dim);
-	if (!final_draw_check(&shape_dim, file, buffer, canva_dim))
+	buffer = background_draw(canva);
+	if (!draw_check(&shape, file, buffer, canva))
 	{
 		write(1, "Error: Operation file corrupted\n", 32);
-		free_buffer(canva_dim, buffer);
+		free_buffer(canva, buffer);
 		fclose(file);
 		return (1);
 	}
-	for (int i = 0; i < canva_dim.height; i++)
+	for (int i = 0; i < canva.height; i++)
 	{
-		for (int j = 0; j < canva_dim.width; j++)
+		for (int j = 0; j < canva.width; j++)
 			printf("%c", buffer[i][j]);
 		printf("\n");
 	}
-	free_buffer(canva_dim, buffer);
+	free_buffer(canva, buffer);
 	fclose(file);
 	return (0);
 }
