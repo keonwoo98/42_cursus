eff999b67fb3ef8456d7f3448eefd2b1809d0ed9
keonwoo98 <keonwoo98@naver.com>
2022-05-26 14:06:27 +0900

    CPP_Module_02

--- CPP_Module/CPP_Module_02/ex03/main.cpp
+++ CPP_Module/CPP_Module_02/ex03/main.cpp
@@ -2,25 +2,55 @@
 
 int main(void)
 {
-	// Point a(8, 3);
-	// Point b(5, 9);
-	// Point c(1, 1);
-	// Point p1(6, 3);
-	// Point p2(4, 1);
-	Point a(0.0, 0.0);
-	Point b(0.0, 4.0);
-	Point c(5.0, 0.0);
-	Point p1(1.0, 1.0);
-	Point p2(-4.234, 1.181);
+	Point a1(8, 3);
+	Point b1(5, 9);
+	Point c1(1, 1);
+	Point p1(4, 4);
+	Point p2(4, 1);
 
-	if (bsp(a, b, c, p1))
+	Point a2(5.5, -2.5);
+	Point b2(2.5, -5.0);
+	Point c2(1.5, -1.0);
+	Point p3(3.5, -3.0);
+	Point p4(5.0, -0.5);
+
+	Fixed x1(8), y1(3);
+	Fixed x2(5), y2(9);
+	Fixed x3(1), y3(1);
+	Fixed x4(4), y4(4);
+	Fixed x5(4), y5(1);
+	Point a3(x1, y1);
+	Point b3(x2, y2);
+	Point c3(x3, y3);
+	Point p5(x4, y4);
+	Point p6(x5, y5);
+
+	if (bsp(a1, b1, c1, p1))
 		std::cout << "p1 is in the triangle" << std::endl;
 	else
 		std::cout << "p1 is out of the triangle" << std::endl;
-	if (bsp(a, b, c, p2))
+	if (bsp(a1, b1, c1, p2))
 		std::cout << "p2 is in the triangle" << std::endl;
 	else
 		std::cout << "p2 is out of the triangle" << std::endl;
+	
+	if (bsp(a2, b2, c2, p3))
+		std::cout << "p3 is in the triangle" << std::endl;
+	else
+		std::cout << "p3 is out of the triangle" << std::endl;
+	if (bsp(a2, b2, c2, p4))
+		std::cout << "p4 is in the triangle" << std::endl;
+	else
+		std::cout << "p4 is out of the triangle" << std::endl;
+
+	if (bsp(a3, b3, c3, p5))
+		std::cout << "p3 is in the triangle" << std::endl;
+	else
+		std::cout << "p3 is out of the triangle" << std::endl;
+	if (bsp(a3, b3, c3, p6))
+		std::cout << "p4 is in the triangle" << std::endl;
+	else
+		std::cout << "p4 is out of the triangle" << std::endl;
 
 	return 0;
 }
