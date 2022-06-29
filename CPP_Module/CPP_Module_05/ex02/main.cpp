35efcd792e12985810723eadf81daad18a62ad23
keonwoo98 <keonwoo98@naver.com>
2022-06-29 20:29:00 +0900

    CPP_Module_05

--- CPP_Module/CPP_Module_05/ex02/main.cpp
+++ CPP_Module/CPP_Module_05/ex02/main.cpp
@@ -4,24 +4,7 @@ int main(void)
 {
 	try
 	{
-		Bureaucrat b1("b1", 30);
-		Form f1("f1", 50, 70);
-		Form f2("f2", 10, 50);
-		Form f3(f1);
-		Form f4 = f2;
-
-		b1.signForm(f1);
-		b1.signForm(f2);
-		std::cout << std::endl;
-
-		std::cout << f1 << std::endl;
-		std::cout << std::endl;
-		std::cout << f2 << std::endl;
-		std::cout << std::endl;
-		std::cout << f3 << std::endl;
-		std::cout << std::endl;
-		std::cout << f4 << std::endl;
-		std::cout << std::endl;
+		
 	}
 	catch(const std::exception& e)
 	{
