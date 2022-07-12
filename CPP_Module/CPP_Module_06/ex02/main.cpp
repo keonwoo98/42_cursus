44629e002b4eb3f38614f86fe12c4ac1ed9af0b6
keonwoo98 <keonwoo98@naver.com>
2022-07-12 16:34:33 +0900

    CPP_Module_06

---
+++ CPP_Module/CPP_Module_06/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main(void)
+{
+	Base* generate(void)
+	{
+		srand(time(NULL));
+		if (rand() % 2)
+	}
+}
