c183d73c0340e5ab7018a704a52d3bf5f847fd04
keonwoo98 <keonwoo98@naver.com>
2022-07-12 19:36:23 +0900

    CPP_Module_06

--- CPP_Module/CPP_Module_06/ex02/main.cpp
+++ CPP_Module/CPP_Module_06/ex02/main.cpp
@@ -5,11 +5,89 @@
 #include <cstdlib>
 #include <ctime>
 
+Base* generate(void)
+{
+	int random;
+
+	srand(time(NULL));
+	random = rand() % 3;
+	std::cout << "<RANDOM>" << std::endl;
+	if (random == 0)
+	{
+		std::cout << "Generated A" << std::endl << std::endl;
+		return new A();
+	}
+	else if (random == 1)
+	{
+		std::cout << "Generated B" << std::endl << std::endl;
+		return new B();
+	}
+	else
+	{
+		std::cout << "Generated C" << std::endl << std::endl;
+		return new C();
+	}
+}
+
+void identify(Base* p)
+{
+	if (A *a = dynamic_cast<A *>(p))
+		std::cout << "A : Identify(Base* p) Success" << std::endl;
+	else
+		std::cout << "A : Identify(Base* p) Fail" << std::endl;
+	if (B *b = dynamic_cast<B *>(p))
+		std::cout << "B : Identify(Base* p) Success" << std::endl;
+	else
+		std::cout << "B : Identify(Base* p) Fail" << std::endl;
+	if (C *c = dynamic_cast<C *>(p))
+		std::cout << "C : Identify(Base* p) Success" << std::endl;
+	else
+		std::cout << "C : Identify(Base* p) Fail" << std::endl;
+	std::cout << std::endl;
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		A &a = dynamic_cast<A &>(p);
+		std::cout << "A : Identify(Base& p) Success" << "\n";
+		(void)a;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "A : Identify(Base& p) Fail" << std::endl;
+	}
+	try
+	{
+		B &b = dynamic_cast<B &>(p);
+		std::cout << "B : Identify(Base& p) Success" << "\n";
+		(void)b;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "B : Identify(Base& p) Fail" << std::endl;
+	}
+	try
+	{
+		C &c = dynamic_cast<C &>(p);
+		std::cout << "C : Identify(Base& p) Success" << "\n";
+		(void)c;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "C : Identify(Base& p) Fail" << std::endl;
+	}
+	std::cout << std::endl;
+}
+
 int main(void)
 {
-	Base* generate(void)
-	{
-		srand(time(NULL));
-		if (rand() % 2)
-	}
+	Base *ptr = generate();
+	std::cout << "<POINTER>" << std::endl;
+	identify(ptr);
+	std::cout << "<REFERENCE>" << std::endl;
+	identify(*ptr);
+
+	delete ptr;
 }
