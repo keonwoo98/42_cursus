56989a037a2fb4f0b62e04cd410a7e0b577602ec
keonwoo98 <keonwoo98@naver.com>
2022-10-08 22:51:10 +0900

    Inception

--- Inception/scrs/requirements/nginx/tools/nginx_start.sh
+++ Inception/scrs/requirements/nginx/tools/nginx_start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl...";
-openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost" -keyout localhost.dev.key -out
+openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost" -keyout localhost.dev.key -out localhost.dev.crt
 echo "Nginx: ssl is set up!";
 fi
 
